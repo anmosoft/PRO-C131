737fe27bd104d149d041c3e27b3dd1d89d5d2e99
anmosoft <72310713+anmosoft@users.noreply.github.com>
2023-11-21 12:47:56 +0530

    calculatingGravity.py

--- calculatingGravity.py
+++ calculatingGravity.py
@@ -3,47 +3,53 @@ import pandas as pd
 
 rows = []
 
-with open("stars.csv",'r') as f:
-    csvreader = csv.reader(f)
-    for row in csvreader:
-        rows.append(row)
+with open('Total_Stars.csv', 'r') as f:
+    csv_r = csv.reader(f)
+    for i in csv_r:
+        rows.append(i)
 
 headers = rows[0]
-star_data = rows[1:]
+stars_data = rows[1:]
+headers[0] = 'Index'
 
-df = pd.read_csv("Total_Stars.csv")
-solar_mass_list = df["solar_mass"].tolist()
-solar_radius_list = df["solar_radius"].tolist()
+star_data = []
 
-solar_mass_list.pop(0)
-solar_radius_list.pop(0)
+for star in stars_data:
+    if star[3] != '?': 
+        star[3] = float(star[3].strip("\'")) * 1.989e+30
+    
+    if star[4] != '?':
+        star[4] = float(star[4].strip("\'")) * 6.957e+8
 
-star_solar_mass_si_unit = []
+    star_data.append(star)
 
-for data in solar_mass_list:
+star_data_gravity = []
 
-    si_unit = float(data)*1.989e+30
-    star_solar_mass_si_unit.append(si_unit)
+for star in star_data:
+    if star[3] != '?' and star[4] != '?':
+        gravity = (6.674e-11 * float(star[3]))/(float(star[4])*float(star[4]))
+    star.append(gravity)
+    star_data_gravity.append(star)
 
-print(star_solar_mass_si_unit)
+name = []
+distance = []
+mass = []
+radius = []
+gravity = []
 
-star_solar_radius_si_unit = []
+for i in star_data_gravity:
+    name.append(i[1])
+    distance.append(i[2])
+    mass.append(i[3])
+    radius.append(i[4])
+    gravity.append(i[5])
 
-for data in solar_radius_list:
-    si_unit = float(data)* 6.957e+8
-    star_solar_radius_si_unit.append(si_unit)
+for i in star_data_gravity:
+    print(i)
 
-print(star_solar_radius_si_unit)
+df = pd.DataFrame(
+    list(zip(name, distance, mass, radius, gravity)),
+    columns=["Star Name", "Distance", "Mass", "Radius", "Gravity"],
+)
 
-star_masses = star_solar_mass_si_unit
-star_radiuses = star_solar_radius_si_unit
-star_names = df["star_names"].tolist()
-star_names.pop(0)
-
-star_gravities = []
-
-for index,data in enumerate(star_names):
-    gravity = (float(star_masses[index])*5.972e+24) / (float(star_radiuses[index])*float(star_radiuses[index])*6371000*6371000) * 6.674e-11
-    star_gravities.append(gravity)
-
-print(star_gravities)
+df.to_csv('final.csv')
